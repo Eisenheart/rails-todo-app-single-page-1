6d9af9e0c345248dff007d6e03eb2b85a2dde105
Marisa Ramon & Teresa Nesteby <pair+mpieces.teresanesteby@devbootcamp.com>
2015-08-20 16:17:20 -0700

    completes ajax to add tasks without refreshing page

--- todo_list/app/controllers/tasks_controller.rb
+++ todo_list/app/controllers/tasks_controller.rb
@@ -9,6 +9,13 @@ class TasksController < ApplicationController
     # respond_to do |format|
     # format.html
     # format.json { render json: @task }
+  end
+  def new
+    @task = Task.new
+  end
 
+  def create
+    @task = Task.create(title: params[:title])
+    render json: @task
   end
 end
